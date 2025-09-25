f1fbc18ec1c01dd3d2acc3efae646af048ada54a
Kiran5258 <skirankumar97849@gmail.com>
2025-09-25 18:37:44 +0530

    frontInstallation

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-center text-red-300 ">Hii</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
