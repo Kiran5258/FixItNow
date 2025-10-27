ef9e7d278f3017027c4aaaa3e26758aab1a81f2e
HARINI150806 <1586harini@gmail.com>
2025-10-27 12:47:29 +0530

    week 6

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -5,7 +5,8 @@ import "./index.css";
 import "leaflet/dist/leaflet.css";
 
 import App from "./App.jsx";
-import UserProvider from "../content/UserProvider.jsx";
+import UserProvider from "./content/Userprovider.jsx";
+
 
 
 createRoot(document.getElementById("root")).render(
